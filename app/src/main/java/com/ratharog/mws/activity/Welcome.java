414f0ff299613f91ef61d70bb0d007a65776899a
rathaROG <siv.ratha89@gmail.com>
2016-11-14 16:23:39 +0700

    - add app icon - update some interface of welcome.xml

--- app/src/main/java/com/ratharog/mws/activity/Welcome.java
+++ app/src/main/java/com/ratharog/mws/activity/Welcome.java
@@ -1,7 +1,10 @@
 package com.ratharog.mws.activity;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.ratharog.mws.R;
 
@@ -16,7 +19,13 @@ public class Welcome extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.welcome);
-    }
 
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(0xFF33B5E5);
+        }
+    }
 
 }
