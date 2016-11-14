e2b41ab82369474d1169d7b3d2e8d49a71372bc9
rathaROG <siv.ratha89@gmail.com>
2016-11-14 12:22:13 +0700

    Initial commit

---
+++ app/src/main/java/com/ratharog/mws/activity/Contact.java
@@ -0,0 +1,8 @@
+package com.ratharog.mws.activity;
+
+/**
+ * Created by rathaROG on 11/14/2016.
+ */
+
+public class Contact {
+}
---
+++ app/src/main/java/com/ratharog/mws/activity/Welcome.java
@@ -0,0 +1,20 @@
+package com.ratharog.mws.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.ratharog.mws.R;
+
+/**
+ * Created by rathaROG on 11/14/2016.
+ */
+
+public class Welcome extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.welcome);
+    }
+}
